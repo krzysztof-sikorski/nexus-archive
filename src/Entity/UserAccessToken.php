e2e8308a0fd20d527df2b25e8da9fae6e62a4734
Krzysztof Sikorski <krzysztof@mksikorscy.pl>
2021-10-27 01:14:51 +0200

    Create Doctrine entity&repository for user access tokens

---
+++ src/Entity/UserAccessToken.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use App\Repository\UserAccessTokenRepository;
+use DateTimeImmutable;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Uid\Uuid;
+
+#[ORM\Entity(repositoryClass: UserAccessTokenRepository::class)]
+final class UserAccessToken
+{
+    #[ORM\Id]
+    #[ORM\Column(type: 'uuid')]
+    private Uuid $id;
+
+    #[ORM\Column(type: 'text', nullable: false)]
+    private ?string $value;
+
+    #[ORM\Column(type: 'datetimetz_immutable', nullable: false)]
+    private ?DateTimeImmutable $createdAt;
+
+    #[ORM\Column(type: 'datetimetz_immutable', nullable: true)]
+    private ?DateTimeImmutable $validUntil;
+
+    public function __construct(?Uuid $id = null)
+    {
+        $this->id = $id ?? Uuid::v4();
+    }
+
+    public function getId(): Uuid
+    {
+        return $this->id;
+    }
+
+    public function setId(Uuid $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getValue(): ?string
+    {
+        return $this->value;
+    }
+
+    public function setValue(string $value): void
+    {
+        $this->value = $value;
+    }
+
+    public function getCreatedAt(): ?DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(DateTimeImmutable $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    public function getValidUntil(): ?DateTimeImmutable
+    {
+        return $this->validUntil;
+    }
+
+    public function setValidUntil(?DateTimeImmutable $validUntil): void
+    {
+        $this->validUntil = $validUntil;
+    }
+}
---
+++ src/Repository/UserAccessTokenRepository.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repository;
+
+use App\Entity\UserAccessToken;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method UserAccessToken|null find($id, $lockMode = null, $lockVersion = null)
+ * @method UserAccessToken|null findOneBy(array $criteria, array $orderBy = null)
+ * @method UserAccessToken[]    findAll()
+ * @method UserAccessToken[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+final class UserAccessTokenRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, UserAccessToken::class);
+    }
+
+    // /**
+    //  * @return UserAccessToken[] Returns an array of UserAccessToken objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('u')
+            ->andWhere('u.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('u.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?UserAccessToken
+    {
+        return $this->createQueryBuilder('u')
+            ->andWhere('u.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
