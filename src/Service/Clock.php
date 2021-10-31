e376acd0a1e9bd5274f15d6bbbb57d7948a37f71
Krzysztof Sikorski <krzysztof@mksikorscy.pl>
2021-10-31 16:03:46 +0100

    Move ClockInterface into a dedicated namespace

--- src/Service/Clock.php
+++ src/Service/Clock.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Service;
 
+use App\Contract\Service\ClockInterface;
 use DateTimeImmutable;
 use DateTimeZone;
 
--- src/Service/UserAccessTokenManager.php
+++ src/Service/UserAccessTokenManager.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Service;
 
+use App\Contract\Service\ClockInterface;
 use App\Entity\UserAccessToken;
 use DateInterval;
 use Doctrine\ORM\EntityManagerInterface;
