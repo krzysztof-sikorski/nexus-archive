821f91fefc13999cfa872fc6e3cb69dae372ad79
Krzysztof Sikorski <krzysztof@mksikorscy.pl>
2021-10-31 18:43:15 +0100

    Add validator method to UserRoles

--- src/Contract/UserRoles.php
+++ src/Contract/UserRoles.php
@@ -4,9 +4,21 @@ declare(strict_types=1);
 
 namespace App\Contract;
 
+use function in_array;
+
 // TODO convert into native enum when PHP 8.1 is released
 final class UserRoles
 {
     public const ROLE_USER = 'ROLE_USER';
     public const ROLE_ADMIN = 'ROLE_ADMIN';
+
+    private const ALL_ROLES = [
+        self::ROLE_USER,
+        self::ROLE_ADMIN,
+    ];
+
+    public static function isValidRole(string $role): bool
+    {
+        return in_array($role, self::ALL_ROLES, true);
+    }
 }
