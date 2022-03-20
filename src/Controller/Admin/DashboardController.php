c82a8a9ca6b7f043358865c718f974e5792e7856
Krzysztof Andrzej Sikorski <krzysztof@mksikorscy.pl>
2022-03-20 23:33:08 +0100

    Generate and configure admin dashboard

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Admin;
+
+use App\Repository\NexusRawDataRepository;
+use App\Repository\UserAccessTokenRepository;
+use App\Repository\UserRepository;
+use DateTimeImmutable;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class DashboardController extends AbstractDashboardController
+{
+    public function __construct(
+        private UserRepository $userRepository,
+        private UserAccessTokenRepository $userAccessTokenRepository,
+        private NexusRawDataRepository $nexusRawDataRepository
+    ) {
+    }
+
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        $userTotalCount = $this->userRepository->getTotalCount();
+        $tokenTotalCount = $this->userAccessTokenRepository->getTotalCount();
+        $dataTotalCount = $this->nexusRawDataRepository->getTotalCount();
+        $dataWeeklyCount = $this->nexusRawDataRepository->getPartialCount(
+            new DateTimeImmutable('midnight UTC - 1 week'),
+            new DateTimeImmutable('midnight UTC')
+        );
+        $dataDailyCount = $this->nexusRawDataRepository->getPartialCount(
+            new DateTimeImmutable('midnight UTC - 1 day'),
+            new DateTimeImmutable('midnight UTC')
+        );
+
+        return $this->render(
+            'admin/dashboard/index.html.twig',
+            [
+                'user_total_count' => $userTotalCount,
+                'token_total_count' => $tokenTotalCount,
+                'data_total_count' => $dataTotalCount,
+                'data_weekly_count' => $dataWeeklyCount,
+                'data_daily_count' => $dataDailyCount,
+            ]
+        );
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Nexus Archive')
+            ->renderContentMaximized()
+            ->generateRelativeUrls();
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+    }
+}
