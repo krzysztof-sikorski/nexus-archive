71a52739c6187b67fa70567dff9177bcee382338
Krzysztof Andrzej Sikorski <krzysztof@mksikorscy.pl>
2022-04-17 22:43:55 +0200

    Remove Symfony Maker bundle because it does not work with modified config

--- config/bundles.php
+++ config/bundles.php
@@ -6,7 +6,6 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
