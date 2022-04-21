6cc6038dad182c04dd55f036b84c702ff9370bbf
Krzysztof Andrzej Sikorski <krzysztof@mksikorscy.pl>
2022-04-21 02:30:45 +0200

    Explicitly set default timezone to UTC

--- src/Kernel.php
+++ src/Kernel.php
@@ -7,7 +7,16 @@ namespace App;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
+use function date_default_timezone_set;
+
 final class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    protected function initializeContainer()
+    {
+        date_default_timezone_set(timezoneId: 'UTC');
+
+        parent::initializeContainer();
+    }
 }
