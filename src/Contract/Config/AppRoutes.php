c6fba257cb447fea466aaaf38ac64b2a15026f45
Krzysztof Andrzej Sikorski <krzysztof@mksikorscy.pl>
2022-04-26 00:11:22 +0200

    Code style: group routes by user visibility

--- src/Contract/Config/AppRoutes.php
+++ src/Contract/Config/AppRoutes.php
@@ -7,11 +7,14 @@ namespace App\Contract\Config;
 // TODO convert into native enum when PHP 8.1 is released
 final class AppRoutes
 {
+    // public routes
     public const HOME = 'app_home';
     public const FAVICON_ICO = 'app_favicon_ico';
     public const LEADERBOARDS = 'app_leaderboards';
+    public const SUBMIT_JSON = 'app_submit_json';
+
+    // undocumented routes
     public const LOGIN = 'app_login';
     public const LOGOUT = 'app_logout';
-    public const SUBMIT_JSON = 'app_submit_json';
     public const EASYADMIN = 'app_easyadmin';
 }
