8e511dc99053ad328489d4399853e0c473d41d79
Krzysztof Andrzej Sikorski <krzysztof@mksikorscy.pl>
2022-04-25 20:37:36 +0200

    Extract main menu to a service class

--- config/packages/twig.php
+++ config/packages/twig.php
@@ -2,11 +2,17 @@
 
 declare(strict_types=1);
 
+use App\Service\MainMenuService;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Config\TwigConfig;
 
+use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
+
 return static function (TwigConfig $twigConfig, ContainerConfigurator $containerConfigurator) {
     $twigConfig->defaultPath(value: '%kernel.project_dir%/templates');
+
+    $mainMenuConfig = $twigConfig->global(key: 'mainMenu');
+    $mainMenuConfig->value(value: service(serviceId: MainMenuService::class));
 
     if ('test' === $containerConfigurator->env()) {
         $twigConfig->strictVariables(value: true);
---
+++ src/Service/MainMenuService.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+use App\Contract\Config\AppRoutes;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+final class MainMenuService
+{
+    public function __construct(private UrlGeneratorInterface $urlGenerator)
+    {
+    }
+
+    public function getMenu(): array
+    {
+        return [
+            [
+                'name' => 'Home',
+                'url' => $this->urlGenerator->generate(name: AppRoutes::HOME),
+                'external' => false,
+            ],
+            [
+                'name' => 'Submit data',
+                'url' => $this->urlGenerator->generate(name: AppRoutes::SUBMIT_JSON),
+                'external' => false,
+            ],
+            [
+                'name' => 'Nexus Clash',
+                'url' => 'https://www.nexusclash.com/',
+                'external' => true,
+            ],
+            [
+                'name' => 'Discord',
+                'url' => 'https://discord.gg/zBVwzD3f8v',
+                'external' => true,
+            ],
+        ];
+    }
+}
